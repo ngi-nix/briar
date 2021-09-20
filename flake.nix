3fcd1cd742fd84444d85aa3d8078e6b19c2d917d
Mazurel <mateusz.mazur@yahoo.com>
2021-09-20 14:52:38 +0200

    Initial work on using android2nix with Briar

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "virtual environments";
+
+  inputs.android2nix.url = "/home/mateusz/ttais/nix/android2nix";
+
+  outputs = { self, android2nix }:
+      android2nix.lib.makeAndroid2nixEnv {
+        mkSrc = { stdenv, ... }: stdenv.mkDerivation {
+          pname = "briar";
+          version = "dev";
+          src = ./.;
+          dontBuild = true;
+          dontConfigure = true;
+
+          installPhase = ''
+            mkdir -p $out
+            cp -rf ./* $out/
+          '';
+
+          fixupPhase = ''
+            cd $out
+
+            find . -name "build.gradle" -exec sed -i "s/classpath files('libs\/gradle-witness\.jar')//" {} \;
+            find . -name "build.gradle" -exec sed -i "s/apply \(plugin\|from\): 'witness\(\.gradle\)\?'//" {} \;
+            find . -name "build.gradle" -exec sed -i "s/id 'witness'//" {} \;
+            # find . -name "build.gradle" -exec sed -i "s/tor 'org.briarproject:obfs4proxy-android:0.0.12-dev-40245c4a@zip'//" {} \;
+          '';
+        };
+        devshell = ./nix/devshell.toml;
+        deps = ./nix/deps.json;
+      };
+}
