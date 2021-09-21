6e41f95f3b09df753fe9a75130b18d8890846386
Mazurel <mateusz.mazur@yahoo.com>
2021-09-21 18:31:30 +0200

    Minor tweaks (doesn't build with tor)

--- flake.nix
+++ flake.nix
@@ -4,30 +4,33 @@
   inputs.android2nix.url = "github:Mazurel/android2nix";
 
   outputs = { self, android2nix }:
-      android2nix.lib.makeAndroid2nixEnv {
-        mkSrc = { stdenv, ... }: stdenv.mkDerivation {
-          pname = "briar";
-          version = "dev";
-          src = ./.;
-          dontBuild = true;
-          dontConfigure = true;
+    android2nix.lib.mkAndroid2nixEnv rec {
+      pname = "briar";
 
-          installPhase = ''
-            mkdir -p $out
-            cp -rf ./* $out/
-          '';
+      mkSrc = { stdenv, ... }: stdenv.mkDerivation {
+        pname = "briar";
+        version = "dev";
+        src = ./.;
+        dontBuild = true;
+        dontConfigure = true;
 
-          fixupPhase = ''
-            cd $out
+        installPhase = ''
+          mkdir -p $out
+          cp -rf ./* $out/
+        '';
 
-            find . -name "build.gradle" -exec sed -i "s/classpath files('libs\/gradle-witness\.jar')//" {} \;
-            find . -name "build.gradle" -exec sed -i "s/apply \(plugin\|from\): 'witness\(\.gradle\)\?'//" {} \;
-            find . -name "build.gradle" -exec sed -i "s/id 'witness'//" {} \;
-            find . -name "build.gradle" -exec sed -i "s/tor 'org.briarproject:obfs4proxy-android:0.0.12-dev-40245c4a@zip'//" {} \;
-          '';
-        };
-        devshell = ./nix/devshell.toml;
-        deps = ./nix/deps.json;
-        buildType = "assembleOfficial";
+        fixupPhase = ''
+          cd $out
+
+          find . -name "build.gradle" -exec sed -i "s/classpath files('libs\/gradle-witness\.jar')//" {} \;
+          find . -name "build.gradle" -exec sed -i "s/apply \(plugin\|from\): 'witness\(\.gradle\)\?'//" {} \;
+          find . -name "build.gradle" -exec sed -i "s/id 'witness'//" {} \;
+          # find . -name "build.gradle" -exec sed -i "s/tor 'org.briarproject:obfs4proxy-android:0.0.12-dev-40245c4a@zip'//" {} \;
+        '';
       };
+
+      devshell = ./nix/devshell.toml;
+      deps = ./nix/deps.json;
+      buildType = "assembleOfficial";
+    };
 }
