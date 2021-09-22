e14732b189c68e46b2752ea9621e2b8881dbbbde
Mazurel <mateusz.mazur@yahoo.com>
2021-09-22 11:52:17 +0200

    Added workaround for fixing obfs4proxy-android issue

--- flake.nix
+++ flake.nix
@@ -4,10 +4,16 @@
   inputs.android2nix.url = "github:Mazurel/android2nix";
 
   outputs = { self, android2nix }:
-    android2nix.lib.mkAndroid2nixEnv rec {
+    android2nix.lib.mkAndroid2nixEnv (
+      { lib, stdenv, gradle_6, jdk11, fetchurl, ... }: {
       pname = "briar";
 
-      mkSrc = { stdenv, ... }: stdenv.mkDerivation {
+      src = let
+        obfs4proxy-android = fetchurl {
+          url = "https://plugins.gradle.org/m2/org/briarproject/obfs4proxy-android/0.0.12-dev-40245c4a/obfs4proxy-android-0.0.12-dev-40245c4a.zip";
+          sha256 = "10pacrw95jin1aj9a3xxasky7n8638l5qribmfsjrglihf7mmc4a";
+        };
+        in stdenv.mkDerivation {
         pname = "briar";
         version = "dev";
         src = ./.;
@@ -21,16 +27,23 @@
 
         fixupPhase = ''
           cd $out
-
+          
+          # Disable Gradle witness
           find . -name "build.gradle" -exec sed -i "s/classpath files('libs\/gradle-witness\.jar')//" {} \;
           find . -name "build.gradle" -exec sed -i "s/apply \(plugin\|from\): 'witness\(\.gradle\)\?'//" {} \;
           find . -name "build.gradle" -exec sed -i "s/id 'witness'//" {} \;
-          # find . -name "build.gradle" -exec sed -i "s/tor 'org.briarproject:obfs4proxy-android:0.0.12-dev-40245c4a@zip'//" {} \;
+
+          # Use prefetched obfs4proxy-android zip as it doesn't work when fetched by gradle, for some reason 
+          sed -i "s|'org.briarproject:obfs4proxy-android:0.0.12-dev-40245c4a@zip'|files('${obfs4proxy-android}')|" ./bramble-android/build.gradle
+
         '';
       };
 
+      gradlePkgs = gradle_6;
+      jdk = jdk11;
+      
       devshell = ./nix/devshell.toml;
       deps = ./nix/deps.json;
       buildType = "assembleOfficial";
-    };
+    });
 }
